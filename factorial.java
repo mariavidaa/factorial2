486679131d78ddb55ffdfe75c896155fe4972096
mariavidaa <mariavida262001@uma.es>
2023-02-08 22:59:33 +0100

    hemos mejorado la apariencia del codigo

--- factorial.java
+++ factorial.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 
 public class factorial {
 
-public static long fact(long num) {
-if (num<=1) {
-return 1;
-} else {
-return num*fact(num-1);
-}
-}
+	public static long fact(long num) {
+		if (num<=1) {
+			return 1;
+		} else {
+			return num*fact(num-1);
+		}
+	}
 	public static void main(String []args) {
-Scanner scanInput = new Scanner(System.in);
-System.out.print("Introduzca un numero: ");
-long num = scanInput.nextLong();
-if (num<0) {
-System.out.println("El factorial no esta definido"
-+ " para numeros negativos");
-} else {
-long fac = fact(num);
-System.out.println("El factorial de "+num+" es "+fac);
-}
-scanInput.close();
-}
+		Scanner scanInput = new Scanner(System.in);
+		System.out.print("Introduzca un numero: ");
+		long num = scanInput.nextLong();
+		if (num<0) {
+			System.out.println("El factorial no esta definido" + " para numeros negativos");
+		} else {
+			long fac = fact(num);
+			System.out.println("El factorial de "+num+" es "+fac);
+		}
+		scanInput.close();
+	}
 
 }
